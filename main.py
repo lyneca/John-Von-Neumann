880eebf2e1318d939845705b1bdc821963511894
wing2048 <atraguliauniliantauthr@gmail.com>
2014-11-11 20:13:24 +1100

    Revert "Structure"
    This reverts commit ce36b446f050e1e02405aa24c2b044a0d3d963d9.

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import pygame
-from constants import *
-pygame.init()
-screen = pygame.display.set_mode(SCREEN_SIZE)
-done = False
-while not done:
-	for event in pygame.event.get():
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_ESCAPE:
-				done = True
-		if event.type == pygame.QUIT:
-			done = True
-pygame.quit()
+# Initial Repository
