ce36b446f050e1e02405aa24c2b044a0d3d963d9
wing2048 <atraguliauniliantauthr@gmail.com>
2014-11-11 20:13:11 +1100

    Structure
    Basic program structure +

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# Initial Repository
+import pygame
+from constants import *
+pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE)
+done = False
+while not done:
+	for event in pygame.event.get():
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				done = True
+		if event.type == pygame.QUIT:
+			done = True
+pygame.quit()
